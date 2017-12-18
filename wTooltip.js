ba43cccb06c8aa78512d6d933f3e1002cf8c46ab
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-12-18 11:49:40 +0600

    Section 2: Plugin class

--- wTooltip.js
+++ wTooltip.js
@@ -1,11 +1,24 @@
 (function($) {
     'use strict';
+
+    function Tooltip(el) {
+        this.$el = el;
+    }
+
     $.fn.wTooltip = function() {
-        console.log("wTooltip called on " + this[0].id);
-        return this;
+
+        function get() {
+            return $.data(this, 'wTooltip') || $.data(this, 'wTooltip', new Tooltip(this));
+        }
+
+        return this.each(get);
     };
 
     $.fn.opacity50 = function() {
-        return $(this).css('opacity', 0.5);  
+        function get() {
+            $(this).css('opacity', 0.5);
+        }
+
+        return $(this).each(get);  
     }
 })(jQuery);
