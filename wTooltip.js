b2f0b8834349d7d4c7b0402f73ce962479f5ffbb
Mir Md. Asif Hossain <mir.md.asif@gmail.com>
2017-12-19 11:38:39 +0600

    Section 2: destroy

--- wTooltip.js
+++ wTooltip.js
@@ -3,15 +3,42 @@
 
     function Tooltip(el) {
         this.$el = el;
-        this.generate();
+        this._generate();
     }
 
-    Tooltip.prototype.generate = function() {
-        // Code here
+    Tooltip.prototype._generate = function() {
+        
+        this.$toolTip = $('<div>Tooltip</div>');
+
+        $('body').append(this.$toolTip);
+
+        this._setColor('red');
+        
+        this.$initialized = true;
     }
+
+    Tooltip.prototype._destroy = function() {
+        
+        this.$toolTip.remove();
+        
+        $.removeData(this.$el, 'wTooltip');
+    }
+
+    Tooltip.prototype._setColor = function(color) {
+        
+        this.$toolTip.css('color', color);
+
+        if(this.initialized) {
+            console.log("Changed color of initialized object");
+        }
+    };
 
     $.fn.wTooltip = function() {
-        // Code here
+        function get() {
+            return $.data(this, 'wTooltip') || $.data(this, 'wTooltip', new Tooltip(this));
+        }
+
+        return this.each(get);
     };
 
     $.fn.opacity50 = function() {
